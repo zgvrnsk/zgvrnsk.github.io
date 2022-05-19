ca849c87830325c96481499a0a0bcaca0f48188e
ZalataG <zlatalabdiz@gmail.com>
2022-05-19 14:21:16 +0000

    eight_ii

--- eight/js/game.js
+++ eight/js/game.js
@@ -33,7 +33,7 @@ export function actions(state)
         return [up, down, left, right]
 }
 
-function successor(state, action)
+export function successor(state, action)
 {
     const newS =[state[0].slice(), state[1].slice(), state[2].slice()]
     const [i, j] = findZero(state)
@@ -74,4 +74,27 @@ export function move(state, i, j)
         return successor(state, down)
     else
         return state
+}
+
+
+export function goalTest(state)
+{
+    const goalState = [
+        [1, 2,3],
+        [4, 5, 6],
+        [7, 8, 0]]
+    return is_equal(goalState, state)
+}
+
+function is_equal(s1, s2)
+{
+    for(let i = 0; i < 3; i++)
+    {
+        for(let j = 0; j < 3; j++)
+        {
+            if(s1[i][j] !== s2[i][j])
+                return false
+        }
+    }
+    return true
 }
--- eight/js/main.js
+++ eight/js/main.js
@@ -1,5 +1,7 @@
 import {GameView, clickToTail, canvas} from "./gameview.js"
-import {move} from "./game.js"
+import {move, goalTest, actions, successor} from "./game.js"
+import {search} from "./search.js"
+
 
 const initialState = [
     [4, 1, 3],
@@ -10,6 +12,8 @@ let s1 = initialState
 new GameView(s1)
 
 canvas.addEventListener('click', moveView)
+const btn_start_ai = document.querySelector('#btn_start_ai')
+btn_start_ai.addEventListener('click', start_ai)
 
 
 function moveView(event)
@@ -18,3 +22,15 @@ function moveView(event)
     s1 = move(s1, i, j)
     new GameView(s1)
 }
+
+function start_ai()
+{
+    const solution = search(s1, goalTest, actions, successor, false)
+    for(let i = 0; i < solution.length; i++)
+    {
+        setTimeout(()=>{new GameView(solution[i].state)}, i * 2000)
+        if(i === solution.length - 1)
+            s1 = solution[i].state
+    }
+}
+
